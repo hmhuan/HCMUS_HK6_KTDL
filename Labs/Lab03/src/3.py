430dc14005219e6e35610a9a93872a6efe97acf4
hmhuan <minhhuanhuynh289@gmail.com>
2019-05-11 22:01:27 +0700

    bar plot Lab03 cau 3

--- Labs/Lab03/src/3.py
+++ Labs/Lab03/src/3.py
@@ -2,16 +2,28 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-# X la numFolds
+# set width of bar
+barWidth = 0.25
+
 X = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
 
 # Y1 la Accuracy voi unpruned = false
 Y1 = np.array([89.1089, 93.0693, 89.1089, 94.0594, 89.1089, 88.1188, 90.099, 92.0792, 92.0792])
 
 # Y2 la Accuracy voi unpruned = true
-Y2 = np.array([92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792])
+Y2 = np.array([92.0792])
 
-plt.plot(X, Y1, X, Y2)
+# Set position of bar on X axis
+r1 = np.arange(2 - barWidth / 2, len(Y1) + 2 - barWidth / 2)
+r2 = [r1[1] + barWidth]
+
+
+#plt.bar(X, Y1, width=barWidth, edgecolor='white', label='var1')
+#plt.bar
+plt.bar(r1, Y1, color='#7f6d5f', width=barWidth, edgecolor='white', label='pruned')
+plt.bar(r2, Y2, color='#557f2d', width=barWidth, edgecolor='white', label='unpruned')
+plt.xticks(X)
 plt.ylabel('Accuracy')
 plt.xlabel('numFolds')
+plt.legend()
 plt.show()
