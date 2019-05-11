78618586bee3e4eed7a6dd8eaa918db1143c4740
hmhuan <minhhuanhuynh289@gmail.com>
2019-05-11 21:35:42 +0700

    create lab04, src read from csv

--- Labs/Lab03/src/3.py
+++ Labs/Lab03/src/3.py
@@ -5,11 +5,11 @@ import math
 # X la numFolds
 X = np.array([2, 3, 4, 5, 6, 7, 8, 9, 10])
 
-# Y la Accuracy voi unpruned = false
-Y1 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9])
+# Y1 la Accuracy voi unpruned = false
+Y1 = np.array([89.1089, 93.0693, 89.1089, 94.0594, 89.1089, 88.1188, 90.099, 92.0792, 92.0792])
 
-#
-Y2 = np.array([3, 1, 9, 9, 0, 3, 2, 8, 10])
+# Y2 la Accuracy voi unpruned = true
+Y2 = np.array([92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792, 92.0792])
 
 plt.plot(X, Y1, X, Y2)
 plt.ylabel('Accuracy')
