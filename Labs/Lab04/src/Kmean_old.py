05729f8826163d054e3e5e5aa34619290d31a844
hmhuan <minhhuanhuynh289@gmail.com>
2019-05-13 22:32:44 +0700

    update code kmean, test with clutser n = 2. need to calc SSE.

--- Labs/Lab04/src/Kmean_old.py
+++ Labs/Lab04/src/Kmean_old.py
@@ -31,62 +31,62 @@ def kMean(data, k):
 	# Inintialize
 	# random k giá trị làm centroid_id và lấy ra các centroid initial
 	initial_id = np.random.randint(100, size = k)
-	#temp = np.array([1, 1, 1, 1, 1, 1, 1, 1])
-	#centroids = [[1,1,1,0,0,1,1,0], [1,1,0,1,0,0,1,1]] # test voi 2 cluster nay
+	temp = np.array([1, 1, 1, 1, 1, 1, 1, 1])
 	centroids = []
 	for id in initial_id:
 		centroids.append(data[id])
 	#for centroid in centroids:
 	#	print(np.sum(np.power(centroid - temp, 2)))
-
+	# empty cluster
 	empty_cluster = [[] for x in range(k)]	
 	
+	#clusters = empty_cluster
+	#for dataPoint in data:
+	#	distances = []
+	#	for centroid in centroids:
+	#		distances.append(distanceMeasurement(dataPoint, centroid))
+	#		#print(np.argmin(distances))
+	#	clusters[np.argmin(distances)].append(dataPoint)
+	#
+	#id = 0
+	#for cluster in clusters:
+	#	for el in cluster:
+	#		print(id, ":", el)
+	#	print("So cluster", id, ":",len(cluster))
+	#	print(np.mean(cluster, axis = 0))
+	#	id += 1
 	clusters = empty_cluster
-	idCluster = np.zeros(len(data), dtype = int)
 	iterations = 0
 	while True:
 		old_centroids = centroids
-		#print(old_centroids)
 		clusters = [[] for x in range(k)]
-		id = 0
-		# Xet cac dataPoint vao cac cluster tuong ung
+		# print(clusters)		
 		for dataPoint in data:
 			distances = []
 			for centroid in centroids:
 				distances.append(distanceMeasurement(dataPoint, centroid))
-			idCluster[id] = np.argmin(distances)
+			# print(np.argmin(distances))
 			clusters[np.argmin(distances)].append(dataPoint)
-			id += 1
-
-		# Cap nhat centroid
 		centroids = []
 		for cluster in clusters:
 			centroids.append(np.mean(cluster, axis = 0))
-			
-		# Neu centroid khong thay doi thi dung
-		#if np.mean(np.absolute(np.array(old_centroids) - np.array(centroids))) < 0.0001:
-		if np.array_equal(old_centroids, centroids) == True:
+		if np.mean(np.absolute(np.array(old_centroids) - np.array(centroids))) < 0.0001:
 			break
+		#print(iterations)
 		iterations += 1
-	#print(centroids)
 	print("Iters =", iterations)
 	
 	id = 0
-	idData = 0
-	nCluster = []
 	for cluster in clusters:
+		count  = 0
 		for el in cluster:
 			print(id, ":", el)
-		nCluster.append(len(cluster))
+			count += 1
 		print("So cluster", id, ":",len(cluster))
 		print(np.mean(cluster, axis = 0))
 		id += 1
 	
-	for dataPoint in data:
-		print(idCluster[idData], ":", dataPoint)
-		idData += 1
-	
-	return centroids, idCluster, nCluster
+	return 0
 
 #
 def write_output_asgn(output_asgn, name, data):
@@ -106,38 +106,18 @@ def write_output_asgn(output_asgn, name, data):
 				line += str(row[i]) + "\n"
 		output_asgn.write(line)
 
-def write_output_model(output_model, name, centroids, nCluster):
-	
-	# ghi SSE
-	
-	# Ghi bang
-	output_model.write("Cluster centroids:\n")
-	output_model.write("\t\t\tCluster#\n")
-	
-	line1, line2 = "Attribute", "         "
-	for i in range(len(nCluster)):
-		line1 += "\t" + str(i)
-		line2 += "\t" + str(nCluster[i])
-	output_model.write(line1 + "\n")
-	output_model.write(line2 + "\n")
-	for i in range(len(name)):
-		line = name[i]
-		for centroid in centroids:
-			line += "\t" + str(round(centroid[i], 4))
-		output_model.write(line + "\n")
-	
-	
-
 def main():
 	fileIn_name, output_model_name, output_asgn_name, k = sys.argv[1], sys.argv[2], sys.argv[3], int(sys.argv[4])
 	nameAttribute, data = read_input_file(fileIn_name)
 	output_model = open(output_model_name, 'w')
 	output_asgn = open(output_asgn_name, 'w')
 	
-	centroids, idCluster, nCluster = kMean(data, k)
+	kMean(data, k)
+	#a = np.array([1, 2])
+	#b = np.array([2, 3])
+	#print(np.array_equal(a, b))
 	# ghi ra file assignment.csv
 	write_output_asgn(output_asgn, nameAttribute, data)
-	write_output_model(output_model, nameAttribute, centroids, nCluster)
 	
 	output_model.close()
 	output_asgn.close()
